894fa501475844fbe8ace42220d8f39f6880456f
Gautam Kumar <gautamkumarxpvt@gmail.com>
2025-09-12 23:23:52 +0530

    adding auth

--- BACKEND/AIYT_summery/summery_generator/views.py
+++ BACKEND/AIYT_summery/summery_generator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout 
+from django.shortcuts import redirect,render
 
 # Create your views here.
 def index(request):
@@ -8,6 +10,25 @@ def user_login(request):
     return render(request, 'login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        usename = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeatPassword = request.POST['repeatPassword']
+        if password == repeatPassword:
+            try:
+                from django.contrib.auth.models import User
+                user = User.objects.create_user(usename, email, password)
+                user.save()
+                return redirect('login')
+            except:
+                error_message = "error in creating user"
+                return render(request, 'signup.html', {'error_message': error_message})
+        else:
+            error_message = "Password and Repeat Password do not match"
+            return render(request, 'signup.html', {'error_message': error_message})
+            
+        
     return render(request, 'signup.html')   
 
 def user_logout(request):
