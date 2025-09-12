7f6ac241d07147bbf7583b902497af6e796fd683
Gautam Kumar <gautamkumarxpvt@gmail.com>
2025-09-13 01:48:52 +0530

    final str ready

--- BACKEND/AIYT_summery/summery_generator/views.py
+++ BACKEND/AIYT_summery/summery_generator/views.py
@@ -1,12 +1,24 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect,render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'index.html')
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = "Invalid username or password"
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
 
 def user_signup(request):
@@ -20,12 +32,12 @@ def user_signup(request):
                 from django.contrib.auth.models import User
                 user = User.objects.create_user(usename, email, password)
                 user.save()
-                return redirect('login')
+                return redirect('/')
             except:
                 error_message = "error in creating user"
                 return render(request, 'signup.html', {'error_message': error_message})
         else:
-            error_message = "Password and Repeat Password do not match"
+            error_message = "Password do not match"
             return render(request, 'signup.html', {'error_message': error_message})
             
         
